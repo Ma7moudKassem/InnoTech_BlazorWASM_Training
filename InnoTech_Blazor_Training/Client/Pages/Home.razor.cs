a23915f14fcb070f1fc5d865c99496ac2ed1be8f
Mahmoud Kassem <modykassem123@gmail.com>
2022-07-02 18:29:44 +0200

    Client Side

---
+++ InnoTech_Blazor_Training/Client/Pages/Home.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace InnoTech_Blazor_Training.Client.Pages
+{
+    public partial class Home
+    {
+        [Inject] public HttpClient _client { get; set; }
+        public Empolyee employee { get; set; } = new();
+        public Student student { get; set; } = new();
+
+        public async Task SaveEmpolyeeToDb() => await _client.PostAsJsonAsync<Empolyee>("api/empolyee", employee);
+        public async Task SaveStudentToDb() => await _client.PostAsJsonAsync<Student>("api/student", student);
+
+        //protected override async Task OnParametersSetAsync()
+        //{
+        //    await client.GetFromJsonAsync<Empolyee>($"api/employees/{id}");
+        //}
+
+    }
+}
---
+++ InnoTech_Blazor_Training/Client/Pages/Students.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace InnoTech_Blazor_Training.Client.Pages
+{
+    public partial class Students
+    {
+        [Inject] public HttpClient _client { get; set; }
+        List<Student>? students = new();
+        public async Task<List<Student>?> GetStudentDataFromDB() => await _client.GetFromJsonAsync<List<Student>>("api/student");
+        protected override async Task OnInitializedAsync() => students = await GetStudentDataFromDB();
+
+    }
+}
