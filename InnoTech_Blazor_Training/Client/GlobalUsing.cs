bcb8cff2ae577f81992a1ae278fd75e4a78904c0
Mahmoud Kassem <modykassem123@gmail.com>
2022-07-02 15:16:51 +0200

    Others

--- InnoTech_Blazor_Training/Client/GlobalUsing.cs
+++ InnoTech_Blazor_Training/Client/GlobalUsing.cs
@@ -2,3 +2,7 @@
 global using System.Net.Http;
 global using Microsoft.AspNetCore.Components;
 global using System.Net.Http.Json;
+global using InnoTech_Blazor_Training.Client;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
--- InnoTech_Blazor_Training/Client/Pages/StudentComponent.razor.cs
+++ InnoTech_Blazor_Training/Client/Pages/StudentComponent.razor.cs
@@ -1,26 +1,22 @@
-﻿
-namespace InnoTech_Blazor_Training.Client.Pages
+﻿namespace InnoTech_Blazor_Training.Client.Pages
 {
-
     public partial class StudentComponent
     {
         [Inject] public HttpClient _client { get; set; }
+
         List<Student>? students = new();
         Student student = new(); 
-
         public async Task SaveStudentDataToDB()
         {
             await _client.PostAsJsonAsync<Student>("api/students" , student);
+             StateHasChanged();
             students = await GetStudentDataFromDB();
-
         }
 
         public async Task<List<Student>?> GetStudentDataFromDB() => await _client.GetFromJsonAsync<List<Student>>("api/students");
 
         protected override async Task OnInitializedAsync() =>
             students = await GetStudentDataFromDB();
-
-
 
     }
 }
